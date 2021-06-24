5013f5e85c9c1b07d5f55fb101cbde220ee7e504
Dhayanandh14 <dhayanandh14@gmail.com>
2021-06-24 19:25:04 +0530

    create order items table

---
+++ db/migrate/20210624135432_create_order_items.rb
@@ -0,0 +1,12 @@
+class CreateOrderItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_items do |t|
+      t.bigint :order_id
+      t.bigint :menu_item_id
+      t.float :price
+      t.float :quantity
+
+      t.timestamps
+    end
+  end
+end
