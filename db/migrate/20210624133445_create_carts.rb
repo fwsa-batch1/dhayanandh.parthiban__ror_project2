67900cda666a2718662cfb47b513a827c7e7622f
Dhayanandh14 <dhayanandh14@gmail.com>
2021-06-24 19:11:07 +0530

    create orders table

--- db/migrate/20210624133445_create_carts.rb
+++ db/migrate/20210624133445_create_carts.rb
@@ -2,8 +2,6 @@ class CreateCarts < ActiveRecord::Migration[6.1]
   def change
     create_table :carts do |t|
       t.bigint :user_id
-
-      t.timestamps
     end
   end
 end
--- db/migrate/20210624133728_create_cart_items.rb
+++ db/migrate/20210624133728_create_cart_items.rb
@@ -4,8 +4,6 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
       t.bigint :cart_id
       t.bigint :menu_item_id
       t.bigint :quantity
-
-      t.timestamps
     end
   end
 end
